c770409dd5a70045acf7c7314ae8799d6e15c669
rafidahrafalia <rafidahlia6@gmail.com>
2020-09-21 14:14:33 +0700

    justify

--- app/src/main/java/com/example/training/service/getAgency.java
+++ app/src/main/java/com/example/training/service/getAgency.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface getAgency {
-    @GET("getAgency")
+    @GET("getAgencyApi")
     Call<Agency> getAgency(@Query("id") String agencyId);
 
     @GET("getUserId")
--- app/src/main/java/com/example/training/ui/DetailProfile/DetailActivity.java
+++ app/src/main/java/com/example/training/ui/DetailProfile/DetailActivity.java
@@ -7,10 +7,12 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -36,6 +38,12 @@ public class DetailActivity extends AppCompatActivity {
 //        mContext=container.getContext();
 //        session=new SessionManager(mContext);
 
+        TextView textView = (TextView) findViewById(R.id.editTextTextMultiLine);
+
+        textView.setText("What is Lorem Ipsum? \\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+                "                \\nWhere does it come from? \\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33. This book is a treatise on the theory of ethics, very popular during the Renaissance.");
+
+        TextJustification.justify(textView);
         textView1  = findViewById(R.id.code);
         textView2  = findViewById(R.id.make);
         textView3  = findViewById(R.id.capacity);
